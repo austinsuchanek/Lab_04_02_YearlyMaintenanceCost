f8bdc6b3cc80c51b221d3b849a86537b571a533d
austinsuchanek <suchanad@mail.uc.edu>
2022-09-18 11:52:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double winter = 483;
+        double spring = 382;
+        double summer = 492;
+        double fall = 402;
+        double yearlyCost;
+
+
+
+        yearlyCost = winter + spring + summer + fall;
+
+        System.out.println("==========================================");
+        System.out.println("Your winter maintenance cost is: " + winter);
+        System.out.println("Your spring maintenance cost is: " + spring);
+        System.out.println("Your summer maintenance cost is: " + summer);
+        System.out.println("Your fall maintenance cost is: " + fall);
+        System.out.println("Your yearly maintenance cost is: " + yearlyCost);
+
+
+    }
+
+}
